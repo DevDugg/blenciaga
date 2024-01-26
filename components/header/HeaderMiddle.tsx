b76de815fa0b79cd1c5cca0bb5634742ac61dac9
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-01-26 17:41:57 +0900

    header padding corrected

--- components/header/HeaderMiddle.tsx
+++ components/header/HeaderMiddle.tsx
@@ -4,7 +4,7 @@ import Tab from "./Tab";
 const HeaderMiddle = () => {
   return (
     <section className="header-middle">
-      <Container className="border-b border-solid border-BLACK pb-2 pt-14">
+      <Container className="border-b border-solid border-BLACK pb-2 pt-14 max-lg:p-0">
         <div className="flex flex-nowrap overflow-x-auto gap-3 items-center px-3">
           <Tab link="#" title="Invierno 23 Hombre" />
           <Tab link="#" title="Invierno 23 Mujer" />
