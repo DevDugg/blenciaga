82021cafd44a8982324b0a5192f88baeb9c617d2
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-01-29 20:08:56 +0900

    ckecks if ls is available

--- utils/cart.ts
+++ utils/cart.ts
@@ -50,7 +50,7 @@ class Cart implements ICartClass {
   // PUBLIC METHODS AND PROPERTIES
 
   public getCartId = () => {
-    return this.cartId || typeof localStorage ? localStorage.getItem("cartId") : null;
+    return this.cartId || typeof localStorage !== "undefined" ? localStorage.getItem("cartId") : null;
   };
 
   public createCart = async (options: ICreateCartMutationOptions) => {
